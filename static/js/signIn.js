1e61f0e140b96a7b59abbae16e00adf1431b10d9
kschandr <mykaysha@gmail.com>
2019-01-27 23:26:16 -0800

    fixed sign up and in button to redirect to success page

--- static/js/signIn.js
+++ static/js/signIn.js
@@ -6,7 +6,8 @@ $(function() {
             data: $('form').serialize(),
             type: 'POST',
             success: function(response) {
-                console.log(response);
+                //console.log(response);
+                window.location.href = "success"
             },
             error: function(error) {
                 console.log(error);
